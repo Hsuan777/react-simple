c2d8441cc148594804198183f88da26c18c67405
Vic <syuan0212@gmail.com>
2024-01-05 00:03:51 +0800

    feat: order

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
-import Count from './Count.tsx'
-import Form from './Form.tsx'
-import List from './List.tsx'
-import './App.css'
+import Count from "./Count.tsx";
+import Form from "./Form.tsx";
+import List from "./List.tsx";
+import Order from "./Order.tsx";
+import "./App.css";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Logo from './Logo';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Logo from "./Logo";
 
 function App() {
   return (
     <>
       <div className="container">
         <Logo title="Welcom to Vite + React!" />
-        <nav className="d-flex justify-content-center mb-3">
-            <a href="#/">Count</a>
-            <a href="#/form" className="mx-5">Form</a>
-            <a href="#/list">List</a>
+        <nav className="d-flex justify-content-center mb-3 gap-3">
+          <a href="#/">Count</a>
+          <a href="#/form">Form</a>
+          <a href="#/list">List</a>
+          <a href="#/order">Order</a>
         </nav>
         <HashRouter>
           <Routes>
             <Route path="/" element={<Count />} />
             <Route path="/form" element={<Form />} />
             <Route path="/list" element={<List />} />
+            <Route path="/order" element={<Order />} />
           </Routes>
         </HashRouter>
-        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Order.tsx
@@ -0,0 +1,19 @@
+function Order() {
+  return (
+    <>
+      <form action="https://ccore.newebpay.com/MPG/mpg_gateway" method="post">
+        <input type="text" name="MerchantID" value="" />
+        <input type="text" name="TradeSha" value="" />
+        <input type="text" name="TradeInfo" value="" />
+        <input type="text" name="TimeStamp" value="" />
+        <input type="text" name="Version" value="2.0" />
+        <input type="text" name="MerchantOrderNo" value="" />
+        <input type="text" name="Amt" value="" />
+        <input type="email" name="Email" value="" />
+        <button type="submit">送出</button>
+      </form>
+    </>
+  );
+}
+
+export default Order;
